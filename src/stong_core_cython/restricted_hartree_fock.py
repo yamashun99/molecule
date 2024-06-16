9435036f27420969d5e96b198d6895d0a9bd3717
yamashun99 <yamashun.103099@gmail.com>
2024-06-16 10:35:51 +0900

    pdos追加

---
+++ src/stong_core_cython/restricted_hartree_fock.py
@@ -0,0 +1,77 @@
+from scipy.linalg import eigh
+import numpy as np
+from md.matrix import *
+from md.two_electron import *
+
+
+class RestrictedHartreeFock:
+    def __init__(self, molecule, basis_set, max_iter=1000, tol=1e-6):
+        self.molecule = molecule
+        self.basis_set = basis_set
+        self.S = None
+        self.T = None
+        self.V = None
+        self.ERI = None
+        self.F = None
+        self.energy = None
+        self.n_electrons = molecule.total_electrons()
+        self.max_iter = max_iter
+        self.tol = tol
+        self.val = None
+        self.vec = None
+
+        # 各種マトリックスの計算
+        matrices = calculate_matrices(self.molecule, self.basis_set)
+        self.S = matrices["Smat"]
+        self.T = matrices["Tmat"]
+        self.V = matrices["Vmat"]
+        self.ERI = matrices["ERImat"]
+
+        self.P = np.zeros((self.S.shape[0], self.S.shape[0]))
+
+    def set_density_matrix(self, P):
+        self.P = P
+
+    def scf(self):
+        # 自己無撞着フィールド(SCF)プロセス
+        print(f"Start SCF for {self.molecule}")
+        old_energy = 0
+        new_energy = 0
+        for i in range(self.max_iter):
+            self.build_fock_matrix()
+            self.val, self.vec = eigh(self.F, self.S)
+            new_P = self.compute_density_matrix(self.vec)
+            self.P = 0.5 * self.P + 0.5 * new_P
+            old_energy = new_energy
+            new_energy = sum(self.val[: self.n_electrons // 2])
+            if abs(new_energy - old_energy) < self.tol:
+                break
+            if i == self.max_iter - 1:
+                print("Did not converge")
+        self.calculate_total_energy()
+
+    def build_fock_matrix(self):
+        # Fock行列を構築
+        hmat = self.T + self.V
+        Jmat = np.einsum("ijkl,kl->ij", self.ERI, self.P)
+        Kmat = np.einsum("ijkl,jk->il", self.ERI, self.P)
+        self.F = hmat + 2 * Jmat - Kmat
+
+    def compute_density_matrix(self, vec):
+        P = np.dot(vec[:, : self.n_electrons // 2], vec[:, : self.n_electrons // 2].T)
+        return P
+
+    def calculate_total_energy(self):
+        h_mat = self.T + self.V
+        one_electron_energy = np.einsum("ij, ij", h_mat, self.P)
+        self.energy = sum(self.val[: self.n_electrons // 2]) + one_electron_energy
+
+    def get_results(self):
+        # 計算結果の取得
+        return {
+            "energy": self.energy,
+            "P": self.P,
+            "val": self.val,
+            "vec": self.vec,
+            # 他にも必要に応じてデータを追加
+        }
--- src/stong_core_cython/unrestricted_hartree_fock.py
+++ src/stong_core_cython/unrestricted_hartree_fock.py
@@ -1,10 +1,12 @@
 from scipy.linalg import eigh
 import numpy as np
 from md.matrix import *
+from scipy.ndimage import gaussian_filter1d
 from md.two_electron import *
+import pandas as pd
 
 
-class HartreeFock:
+class UnrestrictedHartreeFock:
     def __init__(self, molecule, basis_set, max_iter=1000, tol=1e-6):
         self.molecule = molecule
         self.basis_set = basis_set
@@ -14,8 +16,6 @@ class HartreeFock:
         self.ERI = None
         self.F_up = None
         self.F_dn = None
-        self.P_up = None
-        self.P_dn = None
         self.energy = None
         self.n_up, self.n_dn = self.molecule.spin_electron_counts()
         self.max_iter = max_iter
@@ -24,8 +24,8 @@ class HartreeFock:
         self.val_dn = None
         self.vec_up = None
         self.vec_dn = None
+        self.basis_labels = self.get_basis_labels()
 
-    def build_matrices(self):
         # 各種マトリックスの計算
         matrices = calculate_matrices(self.molecule, self.basis_set)
         self.S = matrices["Smat"]
@@ -33,12 +33,24 @@ class HartreeFock:
         self.V = matrices["Vmat"]
         self.ERI = matrices["ERImat"]
 
+        self.P_up = np.ones((self.S.shape[0], self.S.shape[0]))
+        self.P_dn = np.zeros((self.S.shape[0], self.S.shape[0]))
+
+    def get_basis_labels(self):
+        # 基底関数のラベルを取得
+        labels = []
+        for atom_basis in self.basis_set.values():
+            for orbital in atom_basis.keys():
+                labels.append(orbital)
+        return labels
+
+    def set_density_matrix(self, P_up, P_dn):
+        self.P_up = P_up
+        self.P_dn = P_dn
+
     def scf(self):
         # 自己無撞着フィールド(SCF)プロセス
         print(f"Start SCF for {self.molecule}")
-        self.build_matrices()
-        self.P_up = np.ones((self.S.shape[0], self.S.shape[0]))
-        self.P_dn = np.zeros((self.S.shape[0], self.S.shape[0]))
         old_energy = 0
         new_energy = 0
         for i in range(self.max_iter):
@@ -46,12 +58,12 @@ class HartreeFock:
             self.val_up, self.vec_up = eigh(self.F_up, self.S)
             self.val_dn, self.vec_dn = eigh(self.F_dn, self.S)
             new_P_up, new_P_dn = self.compute_density_matrix(self.vec_up, self.vec_dn)
-            self.P_up = 0.9 * self.P_up + 0.1 * new_P_up
-            self.P_dn = 0.9 * self.P_dn + 0.1 * new_P_dn
+            self.P_up = 0.5 * self.P_up + 0.5 * new_P_up
+            self.P_dn = 0.5 * self.P_dn + 0.5 * new_P_dn
             e_up, e_dn = self.error()
             old_energy = new_energy
             new_energy = sum(self.val_up[: self.n_up]) + sum(self.val_dn[: self.n_dn])
-            if abs(new_energy - old_energy) < self.tol:
+            if abs(new_energy - old_energy) < self.tol and i > 1:
                 break
             if i == self.max_iter - 1:
                 print("Did not converge")
@@ -98,6 +110,73 @@ class HartreeFock:
             + sum(self.val_dn[: self.n_dn])
             + one_electron_energy
         ) / 2.0
+
+    def calculate_pdos_weight(self):
+        # PDOS (Partial Density of States) の計算
+        pdos_up = {"energy": self.val_up}
+        pdos_dn = {"energy": self.val_dn}
+
+        for label in self.basis_labels:
+            pdos_up[label] = np.zeros(self.val_up.shape)
+            pdos_dn[label] = np.zeros(self.val_dn.shape)
+
+        for i in range(self.vec_up.shape[1]):
+            for j, label in enumerate(self.basis_labels):
+                pdos_up[label][i] += np.abs(self.vec_up[j, i]) ** 2
+        for i in range(self.vec_dn.shape[1]):
+            for j, label in enumerate(self.basis_labels):
+                pdos_dn[label][i] += np.abs(self.vec_dn[j, i]) ** 2
+        pdos_up = pd.DataFrame(pdos_up)
+        pdos_dn = pd.DataFrame(pdos_dn)
+
+        return pdos_up, pdos_dn
+
+    def calculate_smoothed_pdos(self, sigma=0.1, bins=100):
+        # 平滑化したPDOSの計算
+        pdos_up, pdos_dn = self.calculate_pdos_weight()
+        energy_min = min(np.min(self.val_up), np.min(self.val_dn)) - 1.0
+        energy_max = max(np.max(self.val_up), np.max(self.val_dn)) + 1.0
+        energy_grid = np.linspace(energy_min, energy_max, bins)
+
+        smoothed_pdos_up = {"energy": energy_grid}
+        smoothed_pdos_dn = {"energy": energy_grid}
+
+        for label in self.basis_labels:
+            counts_up, bin_edges = np.histogram(
+                pdos_up["energy"],
+                bins=energy_grid,
+                weights=pdos_up[label],
+                density=True,
+            )
+            counts_dn, _ = np.histogram(
+                pdos_dn["energy"],
+                bins=energy_grid,
+                weights=pdos_dn[label],
+                density=True,
+            )
+            smoothed_counts_up = gaussian_filter1d(counts_up, sigma)
+            smoothed_counts_dn = gaussian_filter1d(counts_dn, sigma)
+            smoothed_pdos_up[label] = smoothed_counts_up
+            smoothed_pdos_dn[label] = smoothed_counts_dn
+
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+        smoothed_pdos_up["energy"] = bin_centers
+        smoothed_pdos_dn["energy"] = bin_centers
+        smoothed_pdos_up = pd.DataFrame(smoothed_pdos_up)
+        smoothed_pdos_dn = pd.DataFrame(smoothed_pdos_dn)
+
+        return smoothed_pdos_up, smoothed_pdos_dn
+
+    def calculate_dos(self, sigma=0.1, bins=100):
+        # DOSの計算
+        levels = np.concatenate((self.val_up, self.val_dn))
+        counts, bin_edges = np.histogram(levels, bins=bins, density=True)
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+        smoothed_counts = gaussian_filter1d(counts, sigma)
+
+        dos = pd.DataFrame({"energy": bin_centers, "dos": smoothed_counts})
+
+        return dos
 
     def get_results(self):
         # 計算結果の取得
